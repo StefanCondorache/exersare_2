730ced17f5f7877bc9884cfe3d8f708c407a1f2d
StefanCondorache <70690185+StefanCondorache@users.noreply.github.com>
2022-11-23 11:12:59 +0300

    Update metoda_Greddy.py

--- metoda_Greddy.py
+++ metoda_Greddy.py
@@ -39,3 +39,34 @@ print(f'suma totala este {suma}')
     #    obiecte_volum.remove(obiecte_volum[obiecte_indice.index(i)])
 
 #print('rucsac - ',rucsac)
+
+
+
+volum_rucsac=float(input('voulumul rucsacului> '))
+rucsac=[]
+
+obiecte_volum=[]
+obiecte_pret=[]
+obiecte_indice=[]
+
+for i in range(int(input('cate obiecte vor fi in total> '))):                       
+    vi=float(input(f'volumul{i+1}> '))
+    pi=float(input(f'pretul{i+1}> '))
+    if vi<=volum_rucsac:
+        obiecte_volum.append(vi)
+        obiecte_pret.append(pi)
+        obiecte_indice.append(pi/vi)
+    else:
+        print(f'obiectul cu volumul {vi} nu va incapea in rucsac cu volumul {volum_rucsac}')
+
+obiecte_volum_2=obiecte_volum.copy()
+obiecte_indice_sort=obiecte_indice.copy()
+obiecte_indice_sort.sort(reverse=True)
+
+for i in obiecte_indice_sort:
+    if sum(rucsac)<=volum_rucsac and sum(rucsac)+obiecte_volum[obiecte_indice.index(i)]<=volum_rucsac :
+        rucsac.append(obiecte_volum[obiecte_indice.index(i)])
+        obiecte_volum.remove(obiecte_volum[obiecte_indice.index(i)])
+
+print('rucsac - ',rucsac)
+print('suma> ',sum([obiecte_pret[obiecte_volum_2.index(i)] for i in rucsac]))
