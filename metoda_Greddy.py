a74ebe63533fa9166233ffda123a184ddfeb2ca3
StefanCondorache <70690185+StefanCondorache@users.noreply.github.com>
2022-11-27 14:02:33 +0200

    Update metoda_Greddy.py

--- metoda_Greddy.py
+++ metoda_Greddy.py
@@ -5,50 +5,6 @@ obiecte_volum=[]
 obiecte_pret=[]
 obiecte_indice=[]
 
-for i in range(int(input('cate obiecte vor fi in total> '))):
-    vi=float(input(f'volumul{i+1}> '))
-    pi=float(input(f'pretul{i+1}> '))
-    if vi<=volum_rucsac:
-        obiecte_volum.append(vi)
-        obiecte_pret.append(pi)
-        obiecte_indice.append(pi/vi)
-    else:
-        print(f'obiectul cu volumul {vi} nu va incapea in rucsac cu volumul {volum_rucsac}')
-
-obiecte_indice_sort=obiecte_indice.copy()
-obiecte_indice_sort.sort(reverse=True)
-
-i=0                                                                                 
-suma=0
-
-while sum(rucsac)<=volum_rucsac and i!=len(obiecte_indice_sort):                                                # <<<<<<<< pentru obiecte infinite
-    if sum(rucsac)+obiecte_volum[obiecte_indice.index(obiecte_indice_sort[i])]<=volum_rucsac:
-        rucsac.append(obiecte_volum[obiecte_indice.index(obiecte_indice_sort[i])])
-        suma+=obiecte_pret[obiecte_indice.index(obiecte_indice_sort[i])]
-    else:
-        i+=1
-
-print(f'volumul total care a incaput este {sum(rucsac)} cu volumele {rucsac}')
-print(f'suma totala este {suma}')
-#print('obiecte_volum - ',obiecte_volum,'\n','obiecte_indice_sort - ',obiecte_indice_sort)                      #<<<<<< pentru obiecte finite
-
-#for i in obiecte_indice_sort:
-    #if sum(rucsac)<=volum_rucsac and sum(rucsac)+obiecte_volum[obiecte_indice.index(i)]<=volum_rucsac :
-    #    rucsac.append(obiecte_volum[obiecte_indice.index(i)])
-    #else:
-    #    obiecte_volum.remove(obiecte_volum[obiecte_indice.index(i)])
-
-#print('rucsac - ',rucsac)
-
-
-
-volum_rucsac=float(input('voulumul rucsacului> '))
-rucsac=[]
-
-obiecte_volum=[]
-obiecte_pret=[]
-obiecte_indice=[]
-
 for i in range(int(input('cate obiecte vor fi in total> '))):                       
     vi=float(input(f'volumul{i+1}> '))
     pi=float(input(f'pretul{i+1}> '))
@@ -58,6 +14,16 @@ for i in range(int(input('cate obiecte vor fi in total> '))):
         obiecte_indice.append(pi/vi)
     else:
         print(f'obiectul cu volumul {vi} nu va incapea in rucsac cu volumul {volum_rucsac}')
+
+for i in range(len(obiecte_volum)-2):
+    for j in range(len(obiecte_pret)-2):
+        if (obiecte_volum[i]/obiecte_pret[i]==obiecte_volum[j+1]/obiecte_pret[j+1]) and (obiecte_volum[i]<obiecte_volum[j+1]):
+            temp_vol=obiecte_volum[j+1]
+            obiecte_volum[j+1]=obiecte_volum[i]
+            obiecte_volum[i]=temp_vol
+            temp_pret=obiecte_pret[j+1]
+            obiecte_pret[j+1]=obiecte_pret[i]
+            obiecte_pret[i]=temp_pret
 
 obiecte_volum_2=obiecte_volum.copy()
 obiecte_indice_sort=obiecte_indice.copy()
@@ -70,3 +36,4 @@ for i in obiecte_indice_sort:
 
 print('rucsac - ',rucsac)
 print('suma> ',sum([obiecte_pret[obiecte_volum_2.index(i)] for i in rucsac]))
+
